2631d443440862636af7841fc5676decf37ffbb1
chavarahul <chavarahul7@gmail.com>
2025-05-18 01:41:41 +0530

    express server

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.CORS_ORIGIN || "http://localhost:3000" }));
+app.use(cors({ origin: '*' }));
 app.use(fileUpload());
 
 // Routes
